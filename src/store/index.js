7db73bc04f680976fff5675f99dbc011d74296e9
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2020-06-21 15:56:44 +0530

    Surge hosting. Changed API to https

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    getURL: "http://corona-xiv.herokuapp.com/?title="
+    getURL: "https://corona-xiv.herokuapp.com/?title="
   },
   mutations: {
   },
